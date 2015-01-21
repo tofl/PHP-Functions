f10c22e444182cfd591b1b4ffdb8b2d043d5cbbe
Tom Flitterman <tom.flitterman@hotmail.fr>
2015-01-21 17:37:08 +0100

    Arranged an array...

--- generateString.php
+++ generateString.php
@@ -3,23 +3,25 @@
 function generateString($length = 30) {
     if (!is_int($length)) return false;
 
-    $char = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    $char =
+        array(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9);
 
     $string = '';
 
     for ($i = 1; $i <= 50; $i++) {
-
         $string .= $char[mt_rand(0, count($char) - 1)];
     }
 
     $nbr_crypts = ceil($length/40);
-
     sha1($string);
 
     for ($i = 1; $i <= $nbr_crypts - 1; $i++) {
         $string .= sha1($string);
     }
-
 
     return substr($string, 0, $length);
 }
