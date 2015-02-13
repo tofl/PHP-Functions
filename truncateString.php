a4cba22790d725b6cd9a0a16e849f2f8c27afe5c
Tom Flitterman <tom.flitterman@hotmail.fr>
2015-02-13 18:21:23 +0100

    Add function "truncateString". More info in the README file.

---
+++ truncateString.php
@@ -0,0 +1,20 @@
+<?php
+
+function truncateString($string, $n) {
+    $n = (int)$n;
+
+    if (!is_string($string) OR !isset($string) OR $n <= 0 OR !isset($n)) return false;
+    if ($n >= strlen($string)) return $string;
+
+    trim($string);
+
+    $string = ' ' . $string . ' ';
+
+    if ($string{$n} != ' ') {
+        while ($string{$n} != ' ') {
+            $n++;
+        }
+    }
+
+    return substr($string, 0, $n) . '...';
+}
