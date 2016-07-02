df43a31cb2439ae041133a966d52df48f6f96bf7
TF <tom.flitterman@hotmail.fr>
2016-07-02 13:07:20 +0200

    Create getThumbnail.php

---
+++ getThumbnail.php
@@ -0,0 +1,13 @@
+<?php
+
+function getThumbnail($video) {
+	
+	if (!isset($video) OR empty($video)) {
+		return false;
+	} elseif (preg_match('#(https?://)?(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9\-_]{11}#', $video)) {	
+		$key_for_video = explode('=', $video);
+		$key_for_video = $key_for_video[1];
+		
+		return $image = 'https://i.ytimg.com/vi/' . $key_for_video . '/hqdefault.jpg';
+	}
+}
