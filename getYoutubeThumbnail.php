2d269dc5d01d4f1ae1ffa3bf1ab9181db4f9d73a
Tom F <thomas.flitterman@ynov.com>
2018-03-28 00:38:39 +0200

    Updated variables to camelCase format

--- getYoutubeThumbnail.php
+++ getYoutubeThumbnail.php
@@ -5,9 +5,9 @@ function getYoutubeThumbnail($video) {
 	if (!isset($video) OR empty($video)) {
 		return false;
 	} elseif (preg_match('#(https?://)?(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9\-_]{11}#', $video)) {	
-		$key_for_video = explode('=', $video);
-		$key_for_video = $key_for_video[1];
+		$keyForVideo = explode('=', $video);
+		$keyForVideo = $key_for_video[1];
 		
-		return $image = 'https://i.ytimg.com/vi/' . $key_for_video . '/hqdefault.jpg';
+		return $image = 'https://i.ytimg.com/vi/' . $keyForVideo . '/hqdefault.jpg';
 	}
 }
